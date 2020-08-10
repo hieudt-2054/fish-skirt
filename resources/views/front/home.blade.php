211a5788365d70093d79db65f68775b6189da0e0
hieudt-2054 <duong.trung.hieu@sun-asterisk.com>
2020-08-10 15:40:55 +0700

    Hot fix msg

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -378,6 +378,30 @@
 
 
 <div class="site-section bg-lightx">
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v7.0'
+      });
+    };
+
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+  <!-- Your Chat Plugin code -->
+  <div class="fb-customerchat"
+    attribution=setup_tool
+    page_id="105993704462484"
+theme_color="#0084ff"
+logged_in_greeting="Chào bạn chúng tôi có thể giúp gì cho bạn"
+logged_out_greeting="Chào bạn chúng tôi có thể giúp gì cho bạn">
+  </div>
     <div class="container">
       <div class="row justify-content-center text-center">
         <div class="col-md-5">
