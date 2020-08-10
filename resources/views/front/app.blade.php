92409afdcc17c6df818ea0d30fe5c42f65e17e9f
hieudt-2054 <duong.trung.hieu@sun-asterisk.com>
2020-08-10 16:16:11 +0700

    Hot fix msg

--- resources/views/front/app.blade.php
+++ resources/views/front/app.blade.php
@@ -26,11 +26,37 @@
   <link rel="stylesheet" href="https://md-aqil.github.io/images/swiper.min.css">
    </head>
    <body>
-
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v7.0'
+        });
+      };
+  
+      (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+  
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat"
+      attribution=setup_tool
+      page_id="105993704462484"
+  theme_color="#0084ff"
+  logged_in_greeting="Chào bạn chúng tôi có thể giúp gì cho bạn"
+  logged_out_greeting="Chào bạn chúng tôi có thể giúp gì cho bạn">
+    </div>
   
   <!-- Back to top button -->
   <a id="button"></a>
-
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v7.0"
+      nonce="WxNHVrsz"></script>
     <div id="loader-wrapper">
       <div id="loader"></div>
   
